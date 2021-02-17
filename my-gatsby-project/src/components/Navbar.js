5b9b58c388b4f5a74be0966c232ac7d3d63bf2ac
wanzyrowan <evaldas542@gmail.com>
2021-02-17 06:37:39 +0000

    run npm install first - logo updated they need to make one tho

--- my-gatsby-project/src/components/Navbar.js
+++ my-gatsby-project/src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="p and s gas engineers" style={{ width: '200px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
