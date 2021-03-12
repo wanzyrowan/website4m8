81a08d13ec89a8e4a97f06b6fdbfc79b795ab37d
wanzyrowan <evaldas542@gmail.com>
2021-03-12 11:19:15 +0000

    basic text updates

--- components/Banner.js
+++ components/Banner.js
@@ -2,11 +2,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Trusted Gas Safe Engineers</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Our Engineers are Gas Safe registered, fully licensed and insured.<br />
+                We have helped thousands of customers accross Greater London and Home Counties.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Header = (props) => (
     <header id="header" className="alt">
         <Link href="/">
-            <a className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></a>
+            <a className="logo"><strong>P&amp;S</strong> <span>Gas Safe Engineers</span></a>
         </Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
