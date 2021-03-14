e8ee65a76e53230a20eb4b4fe351507a9e4916a5
wanzyrowan <evaldas542@gmail.com>
2021-03-14 00:31:14 +0000

    fixed menu

--- components/Menu.js
+++ components/Menu.js
@@ -6,13 +6,14 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link href="/"><a><span onClick={props.onToggleMenu}>Home</span></a></Link></li>
-                <li><Link href="/landing"><a><span onClick={props.onToggleMenu}>Landing</span></a></Link></li>
-                <li><Link href="/generic"><a><span onClick={props.onToggleMenu}>Generic</span></a></Link></li>
-                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Elements</span></a></Link></li>
+                <li><Link href="/landing"><a><span onClick={props.onToggleMenu}>Why Us</span></a></Link></li>
+                <li><Link href="/generic"><a><span onClick={props.onToggleMenu}>Services</span></a></Link></li>
+                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Covered Areas</span></a></Link></li>
+                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Blog</span></a></Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+                <li><a href="#" className="button special fit">Contact Us</a></li>
+                <li><a href="#" className="button fit">FAQs</a></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
