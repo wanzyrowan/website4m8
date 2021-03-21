7e01c104367339e727924934de640a82deffd08e
wanzyrowan <evaldas542@gmail.com>
2021-03-21 18:57:50 +0000

    paging changes + menu

--- components/Menu.js
+++ components/Menu.js
@@ -7,13 +7,13 @@ const Menu = (props) => (
             <ul className="links">
                 <li><Link href="/"><a><span onClick={props.onToggleMenu}>Home</span></a></Link></li>
                 <li><Link href="/meetus"><a><span onClick={props.onToggleMenu}>Why Us</span></a></Link></li>
-                <li><Link href="/generic"><a><span onClick={props.onToggleMenu}>Services</span></a></Link></li>
-                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Our Rates</span></a></Link></li>
-                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Covered Areas</span></a></Link></li>
+                <li><Link href="/services"><a><span onClick={props.onToggleMenu}>Services</span></a></Link></li>
+                <li><Link href="/rates"><a><span onClick={props.onToggleMenu}>Our Rates</span></a></Link></li>
+                <li><Link href="/areas"><a><span onClick={props.onToggleMenu}>Covered Areas</span></a></Link></li>
             </ul>
             <ul className="actions vertical">
                 <li><a href="#" className="button special fit">Contact Us</a></li>
-                <li><a href="#" className="button fit">FAQs</a></li>
+                <li><a href="/faq" className="button fit">FAQs</a></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
