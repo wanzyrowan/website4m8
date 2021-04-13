b1add8fe237fcb9e1004afaf49d762fd57a3e7d0
wanzyrowan <evaldas542@gmail.com>
2021-04-13 21:44:27 +0000

    added pages for each service / copie of "rates"

---
+++ pages/service-and-repairs.js
@@ -0,0 +1,72 @@
+import Head from "next/head"
+
+import Typist from 'react-typist';
+
+import Layout from '../components/Layout'
+import Banner from '../components/Banner'
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>P&amp;S Gas Safe Engineers | Our Rates</title>
+            <meta name="description" content="Elements Page" />
+        </Head>
+
+        <Banner textDisable={true} styleClasses="style2 aboutPage" />
+        <div id="main">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1><Typist cursor={{hideWhenDone: true, hideWhenDoneDelay: 1}}>Our Rates</Typist></h1>
+                    </header>
+                    <div className="table-wrapper">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Item1</td>
+                                    <td>Ante turpis integer aliquet porttitor.</td>
+                                    <td>29.99</td>
+                                </tr>
+                                <tr>
+                                    <td>Item2</td>
+                                    <td>Vis ac commodo adipiscing arcu aliquet.</td>
+                                    <td>19.99</td>
+                                </tr>
+                                <tr>
+                                    <td>Item3</td>
+                                    <td> Morbi faucibus arcu accumsan lorem.</td>
+                                    <td>29.99</td>
+                                </tr>
+                                <tr>
+                                    <td>Item4</td>
+                                    <td>Vitae integer tempus condimentum.</td>
+                                    <td>19.99</td>
+                                </tr>
+                                <tr>
+                                    <td>Item5</td>
+                                    <td>Ante turpis integer aliquet porttitor.</td>
+                                    <td>29.99</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colSpan="2"></td>
+                                    <td>100.00</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </section>
+            <section className="orange">
+            </section>
+        </div>
+    </Layout>
+)
