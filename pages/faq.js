f2aa8fb4724c858b908c93690494ffd3c6cda33d
wanzyrowan <evaldas542@gmail.com>
2021-04-15 22:19:17 +0000

    removed this accordation

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 
 import Layout from '../components/Layout'
-import Accordion from 'react-responsive-accordion'
+
 
 export default () => (
     <Layout>
@@ -11,19 +11,7 @@ export default () => (
         </Head>
 
         <div id="main" className="alt">
-            <Accordion>
-                <div data-trigger="A nifty React accordion component">
-                    <p>So this is an Accordion component that used the <a href="https://github.com/glennflanagan/react-collapsible">react-collapsible</a> component. How handy.</p>
-                </div>
-        
-                <div data-trigger="What the difference?" data-trigger-when-open="THAT is the difference!">
-                    <p>An Accordion is different to a Collapsible in the sense that only one "tray" will be open at any one time.</p>
-                </div>
-        
-                <div data-trigger="I'm responsive and I have a little secret. Look inside.">
-                    <p>And this Accordion component is also completely repsonsive. Hurrah for mobile users!</p>
-                </div>
-            </Accordion>
+
         </div>
     </Layout>
 )
