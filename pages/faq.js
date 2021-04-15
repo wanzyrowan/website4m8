a48a1cb531b996c6d33b774e2a21abef1d7d5eb8
wanzyrowan <evaldas542@gmail.com>
2021-04-15 22:36:11 +0000

    added faq system just need to add actual texts

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 
 import Layout from '../components/Layout'
+import Collapsible from 'react-collapsible';
 
 
 export default () => (
@@ -11,7 +12,20 @@ export default () => (
         </Head>
 
         <div id="main" className="alt">
-
+            <section id="one">
+                <div className="inner">
+                    <Collapsible trigger="Wut?">
+                        <p>
+                            This is the collapsible content. It can be any element or React
+                            component you like.
+                        </p>
+                        <p>
+                            It can even be another Collapsible component. Check out the next
+                            section!
+                        </p>
+                    </Collapsible>
+                </div>
+            </section>
         </div>
     </Layout>
 )
